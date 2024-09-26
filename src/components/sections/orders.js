ce21fb690e1fa88f9274fe36ebaf9ff9aafb077f
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-09-26 09:27:21 +0200

    orders management added

--- src/components/sections/orders.js
+++ src/components/sections/orders.js
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './section_style.scss';
 
-
 const Orders = () => {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        const response = await axios.get('https://vikingsdb.up.railway.app/orders/');
+        const token = localStorage.getItem('token');
+        const response = await axios.get('https://vikingsdb.up.railway.app/orders/', {
+          headers: {
+            Authorization: `Bearer ${token}` // Añade el token a los encabezados
+          }
+        });
         setOrders(response.data);
       } catch (error) {
         console.error('Error fetching orders data', error);
@@ -26,20 +30,18 @@ const Orders = () => {
         <ul>
             <li className="header">
             <span>ID</span>
-            <span>Cliente</span>
-            <span>Producto</span>
-            <span>Cantidad</span>
-            <span>Precio Total</span>
+            <span>Usuario</span>
             <span>Fecha</span>
+            <span>Total</span>
+            <span>Estado</span>
             </li>
             {orders.map((order) => (
             <li key={order.id}>
                 <span>{order.id}</span>
-                <span>{order.customer}</span>
-                <span>{order.product}</span>
-                <span>{order.quantity}</span>
-                <span>{order.totalPrice}</span>
-                <span>{order.date}</span>
+                <span>{order.user_id}</span>
+                <span>{order.order_date}</span>
+                <span>{order.total_price}</span>
+                <span>{order.status}</span>
             </li>
             ))}
         </ul>
