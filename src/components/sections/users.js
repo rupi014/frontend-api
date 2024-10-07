68898225cb27dce2bd57053dac6deb0b077a8d11
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-10-07 17:47:35 +0200

    edut users and mq added

--- src/components/sections/users.js
+++ src/components/sections/users.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './users_style.css';
+import './users_style.scss';
 
 const Users = () => {
   const [userId, setUserId] = useState('');
   const [userData, setUserData] = useState(null);
   const [errorMessage, setErrorMessage] = useState('');
+  const [userToEdit, setUserToEdit] = useState(null);
+  const [newUser, setNewUser] = useState({
+    username: '',
+    email: '',
+    telephone: '',
+    address: '',
+    role: ''
+  });
 
   const getAuthToken = () => {
     return localStorage.getItem('token');
@@ -21,22 +29,85 @@ const Users = () => {
       });
       if (response.data) {
         setUserData(response.data);
-        setErrorMessage(''); // Limpiar el mensaje de error si se encuentra el usuario
+        setErrorMessage('');
       } else {
         setUserData(null);
         setErrorMessage(`El usuario ${userId} no existe`);
       }
     } catch (error) {
-      console.error('Error fetching user data:', error);
-      setUserData(null);
-      setErrorMessage(`El usuario ${userId} no existe`);
+      if (error.response && error.response.status === 401) {
+        setErrorMessage('No autorizado. Por favor, inicia sesión.');
+      } else {
+        console.error('Error fetching user data:', error);
+        setUserData(null);
+        setErrorMessage(`El usuario ${userId} no existe`);
+      }
     }
+  };
+
+  const handleDeleteUser = async (id) => {
+    const confirmDelete = window.confirm("¿Estás seguro de que deseas eliminar este usuario?");
+    if (!confirmDelete) return;
+
+    try {
+      const token = getAuthToken();
+      await axios.delete(`https://vikingsdb.up.railway.app/users/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setUserData(null);
+      setUserToEdit(null);
+      setErrorMessage(`Usuario ${id} eliminado exitosamente.`);
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        setErrorMessage('No autorizado. Por favor, inicia sesión.');
+      } else {
+        console.error('Error deleting user', error);
+      }
+    }
+  };
+
+  const handleEditUser = (user) => {
+    if (userToEdit && userToEdit.id === user.id) {
+      setUserToEdit(null);
+      setNewUser({ username: '', email: '', telephone: '', address: '', role: '' });
+    } else {
+      setUserToEdit(user);
+      setNewUser(user);
+    }
+  };
+
+  const handleUpdateUser = async () => {
+    try {
+      const token = getAuthToken();
+      const updatedUser = await axios.put(`https://vikingsdb.up.railway.app/users/${userToEdit.id}`, newUser, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setUserData(updatedUser.data);
+      setUserToEdit(null);
+      setNewUser({ username: '', email: '', telephone: '', address: '', role: '' });
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        setErrorMessage('No autorizado. Por favor, inicia sesión.');
+      } else {
+        console.error('Error updating user', error);
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewUser({ ...newUser, [name]: value });
   };
 
   return (
     <div className="users-container">
       <h2>Buscar Usuario</h2>
       <input
+        className='search-input'
         type="text"
         placeholder="Introduce el ID del usuario"
         value={userId}
@@ -54,6 +125,7 @@ const Users = () => {
               <th>Teléfono</th>
               <th>Dirección</th>
               <th>Rol</th>
+              <th>Acciones</th>
             </tr>
           </thead>
           <tbody>
@@ -64,9 +136,36 @@ const Users = () => {
               <td data-label="Teléfono">{userData.telephone}</td>
               <td data-label="Dirección">{userData.address}</td>
               <td data-label="Rol">{userData.role}</td>
+              <td>
+                <button
+                  className={`edit-button ${userToEdit && userToEdit.id === userData.id ? 'hover' : ''}`}
+                  onClick={() => handleEditUser(userData)}
+                >
+                  {userToEdit && userToEdit.id === userData.id ? 'Cancelar' : 'Editar'}
+                </button>
+                <button className='delete-button' onClick={() => handleDeleteUser(userData.id)}>Eliminar</button>
+              </td>
             </tr>
           </tbody>
         </table>
+      )}
+      {userToEdit && (
+        <div className="edit-section">
+          <h2>Editar Usuario</h2>
+          <form className="edit-form">
+            <label>Nombre de Usuario:</label>
+            <input type="text" name="username" value={newUser.username} onChange={handleChange} />
+            <label>Email:</label>
+            <input type="email" name="email" value={newUser.email} onChange={handleChange} />
+            <label>Teléfono:</label>
+            <input type="text" name="telephone" value={newUser.telephone} onChange={handleChange} />
+            <label>Dirección:</label>
+            <input type="text" name="address" value={newUser.address} onChange={handleChange} />
+            <label>Rol:</label>
+            <input type="text" name="role" value={newUser.role} onChange={handleChange} />
+            <button type="button" className='update-button' onClick={handleUpdateUser}>Actualizar</button>
+          </form>
+        </div>
       )}
     </div>
   );
